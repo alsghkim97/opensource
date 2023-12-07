c79141904c91903e3a119c35a2bc55438c6b8443
Kim Minho <alsghkim97@naver.com>
2023-12-07 12:35:32 +0900

    commit 5
    "  " 레이블 추가
     -바탕색: #E0E0E0
     -결과값 출력위한 레이블

--- op.py
+++ op.py
@@ -22,6 +22,9 @@ f1.pack(fill="both",expand=True)
 
 resultLabel1 = Label(f1,text="결과:",bg="#E0E0E0")
 resultLabel1.place(x=260,y=270)
+resultLabel2 = Label(f1,text="      ",bg="#E0E0E0")
+resultLabel2.place(x=290,y=270)
+
 
 
 window.mainloop()
